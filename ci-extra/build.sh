b3602549c716aaf081e8ccb845b73619d5b515f6
sefrolov <sefrolov@CompB.localdomain>
2025-09-27 21:36:48 +0300

    IWYU addition for cpp tasks

--- ci-extra/build.sh
+++ ci-extra/build.sh
@@ -9,4 +9,8 @@ cmake -S . \
   -D CT_TREAT_WARNINGS_AS_ERRORS=ON
 
 # Build
-cmake --build "build/${PRESET_NAME}" -j
+cmake --build "build/${PRESET_NAME}" -j | tee "build/build_log.out"
+
+if grep -Eq "include-what-you-use reported diagnostics" "build/build_log.out"; then
+  exit 1
+fi
